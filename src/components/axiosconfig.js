de1ebd509fc4a3da93771cd6d53a97bff69f3159
Velmoragroup <velmoraholdingsgroup@gmail.com>
2026-09-14 16:58:25 +0300

    connected the axiosconfig to theither local host or render server

--- src/components/axiosconfig.js
+++ src/components/axiosconfig.js
@@ -1,17 +1,75 @@
+// import axios from 'axios';
+// import { getAccessToken, getRefreshToken, setTokens, clearTokens } from './tokenutils';
+
+// const api = axios.create({
+//     baseURL: 'http://localhost:8000',
+//     headers: {
+//         'Content-Type': 'application/json',
+//       },
+// });
+
+// api.interceptors.request.use(
+
+//     // console.log("request going out")
+
+//     config => {
+//         const token = getAccessToken();
+//         if (token) {
+//             config.headers.Authorization = `Bearer ${token}`;
+//         }
+//         return config;
+//     },
+//     // console.log("About to make Request ", error),
+//     error => Promise.reject(error)
+// );
+
+// api.interceptors.response.use(
+//     response => response,
+//     async error => {
+//         console.log("Error Response from Server " , error)
+//         const originalRequest = error.config;
+//         if (error.response.status === 401 && !originalRequest._retry) {
+//             console.log("Retrying Request")
+//             originalRequest._retry = true;
+//             const refreshToken = getRefreshToken();
+//             if (refreshToken) {
+//                 try {
+//                     console.log("Now trying to get Access toeken using Refresh token");
+                    
+//                     const response = await axios.post('http://localhost:8000/o/auth/refreshtokens', { refreshToken });
+//                     setTokens(response.data.accessToken, response.data.refreshToken);
+//                     axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.accessToken}`;
+//                     return api(originalRequest);
+//                 } catch (e) {
+//                     clearTokens();
+//                     window.location.href = '/signin';
+//                 }
+//             }
+//         }
+//         return Promise.reject(error);
+//     }
+// );
+
+// export default api;
+
+
+
+
+
 import axios from 'axios';
 import { getAccessToken, getRefreshToken, setTokens, clearTokens } from './tokenutils';
 
+// Dynamically target process.env or fallback to localhost
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
-      },
+    },
 });
 
 api.interceptors.request.use(
-
-    // console.log("request going out")
-
     config => {
         const token = getAccessToken();
         if (token) {
@@ -19,26 +77,31 @@ api.interceptors.request.use(
         }
         return config;
     },
-    // console.log("About to make Request ", error),
     error => Promise.reject(error)
 );
 
 api.interceptors.response.use(
     response => response,
     async error => {
-        console.log("Error Response from Server " , error)
+        console.log("Error Response from Server ", error);
         const originalRequest = error.config;
-        if (error.response.status === 401 && !originalRequest._retry) {
-            console.log("Retrying Request")
+
+        if (error.response && error.response.status === 401 && !originalRequest._retry) {
+            console.log("Retrying Request");
             originalRequest._retry = true;
             const refreshToken = getRefreshToken();
+
             if (refreshToken) {
                 try {
-                    console.log("Now trying to get Access toeken using Refresh token");
+                    console.log("Now trying to get Access token using Refresh token");
                     
-                    const response = await axios.post('http://localhost:8000/o/auth/refreshtokens', { refreshToken });
+                    // Uses dynamic BASE_URL instead of hardcoded localhost
+                    const response = await axios.post(`${BASE_URL}/o/auth/refreshtokens`, { refreshToken });
                     setTokens(response.data.accessToken, response.data.refreshToken);
-                    axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.accessToken}`;
+                    
+                    api.defaults.headers.common['Authorization'] = `Bearer ${response.data.accessToken}`;
+                    originalRequest.headers['Authorization'] = `Bearer ${response.data.accessToken}`;
+                    
                     return api(originalRequest);
                 } catch (e) {
                     clearTokens();
